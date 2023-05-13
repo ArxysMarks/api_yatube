0002021467abe6260fdd42a4ce9bb6c1e650bf19
ArxysMarks <nva.dom@mail.ru>
2023-05-14 03:31:44 +0700

    1

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,26 @@
+from api.views import CommentViewSet, GroupViewSet, PostViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, 'comments')
+
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
